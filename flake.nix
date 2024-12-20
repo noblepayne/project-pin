a375e8490f98ddf5f47d3d0c4f0944aebad7fa67
Wes Payne <noblepayne@noblepayne.com>
2024-12-19 18:36:04 -0800

    initial flake

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux"];
+    pkgsBySystem = nixpkgs.lib.getAttrs supportedSystems nixpkgs.legacyPackages;
+    forAllPkgs = fn: nixpkgs.lib.mapAttrs (system: pkgs: (fn pkgs)) pkgsBySystem;
+  in {
+    formatter = forAllPkgs (pkgs: pkgs.alejandra);
+  };
+}
